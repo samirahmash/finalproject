1d7e84057780f50fa1d9adc0732f5159f14be028
eyeneye <nathanbombardier@d-140-142-161-180.dhcp4.washington.edu>
2016-11-29 18:57:18 -0800

    added server.R file

---
+++ server.R
@@ -0,0 +1,7 @@
+# Final Project Team 4
+
+# data from International Labor Organization on unemployment from 1990 to 2015, 
+# broken down by country, gender, age group, urban/rural, year
+# NOTE: Can't figure out how to make a general and shortened file path for some reason...
+data <- read.csv("/Users/nathanbombardier/Documents/School/Fall2016/INFO201/finalproject/data/ilodata.csv")
+
