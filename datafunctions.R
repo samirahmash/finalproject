6af46d792fcc1e126e5048958b0e816beb10977a
eyeneye <nathanbombardier@d-69-91-233-110.dhcp4.washington.edu>
2016-11-30 17:33:17 -0800

    added datafunctions.R

---
+++ datafunctions.R
@@ -0,0 +1,24 @@
+# data from International Labor Organization on unemployment from 1990 to 2015, 
+# broken down by country, gender, age group, urban/rural, year
+# NOTE: Can't figure out how to make a general and shortened file path for some reason...
+data <- read.csv("/Users/nathanbombardier/Documents/School/Fall2016/INFO201/finalproject/data/ilodata.csv")
+
+# load packages
+library(dplyr)
+
+# pare down columns in dataframe to just country, sex, age, urban status, year, and unemployment
+short.data <- select(data, Country_Label, Sex_Item_Label, 
+                     Classif1_Item_Label, Classif2_Item_Label, Time, Obs_Value)
+
+
+# filter down results to both sexes, urban and rural, all ages, and latest year data was collected
+mapping.data <- filter(short.data, Classif2_Item_Label == 'National') %>% 
+  filter(Sex_Item_Label == "Total") %>%
+  filter(Classif1_Item_Label == "Total") %>% 
+  group_by(Country_Label) %>% 
+  filter(Time == max(Time))
+
+# for some reason there are duplicates, this deals with that
+mapping.data <- mapping.data[!duplicated(mapping.data), ]
+
+View(mapping.data)
