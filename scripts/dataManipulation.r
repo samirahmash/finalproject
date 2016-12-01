fd03508101c4bb3921833ada6d4e001b68beda10
mackmor13 <mackaay@uw.edu>
2016-11-30 20:36:50 -0800

    added libaries

--- scripts/dataManipulation.r
+++
@@ -1,4 +0,0 @@
-library(dplyr)
-setwd("~/Info_201/finalproject/scripts")
-dataFinal <- read.csv("../data/ilodata.csv", stringsAsFactors = FALSE)
-
--- server.R
+++ server.R
@@ -3,7 +3,11 @@
 # data from International Labor Organization on unemployment from 1990 to 2015, 
 # broken down by country, gender, age group, urban/rural, year
 # NOTE: Can't figure out how to make a general and shortened file path for some reason...
-data <- read.csv("/Users/nathanbombardier/Documents/School/Fall2016/INFO201/finalproject/data/ilodata.csv")
+library(dplyr)
+library(rsconnect)
+
+
+data <- read.csv("./data/ilodata.csv")
 
 shinyServer(function(input, output) { 
   # Render a plotly object that returns your scatter on the UI's radio button and select indicator
