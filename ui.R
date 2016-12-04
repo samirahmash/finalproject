7e6234f7b845eb62ade9a685840abdd14ed6c07f
Gabe Bizar <Gabe Bizar>
2016-12-03 16:35:40 -0800

    12/3 4:35 update, still need fixes

--- ui.R
+++ ui.R
@@ -6,37 +6,54 @@ df <- read.csv("./data/ilodata.csv", stringsAsFactors = FALSE)
 only.countries <- select(df, Country_Label) %>% unique()
 only.ages <- select(df, Classif1_Item_Label) %>% unique()
 shinyUI(fluidPage(
-  titlePanel("International Labor Organization - Visualized"),
-  sidebarLayout(
-    sidebarPanel(
-      radioButtons("radio1","Gender", 
-                   choices = list("Female",
-                                  "Male",
-                                  "Total"),
-                   selected = "Total"),
-      
-      radioButtons("radio2", "Area",
-                    choices = list("Rural",
-                                   "Urban", 
-                                   "National"),
-                    selected = "National"),
-    
-      sliderInput("slider2", "Slider Range", 
-                  min = min(df$Time), 
-                  max = max(df$Time), 
-                  value = c(1995, 1996),
-                  round = TRUE,
-                  step = 1,
-                  animate = TRUE
-                  ),
-      
-      selectInput("select", label = h3("Select an Age Range"),
-                  choices = only.ages, selected = only.ages[1])
-    ),
-    mainPanel(
-      textOutput("text1"),
-      plotlyOutput("GlobalMap")
+  tabsetPanel(
+    tabPanel("International Labor Organization - Visualized",
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons("radio1","Gender", 
+                       choices = list("Female",
+                                      "Male",
+                                      "Total"),
+                       selected = "Total"),
+          
+          radioButtons("radio2", "Area",
+                        choices = list("Rural",
+                                       "Urban", 
+                                       "National"),
+                        selected = "National"),
+        
+          sliderInput("slider2", "Slider Range", 
+                      min = min(df$Time), 
+                      max = max(df$Time), 
+                      value = c(1995, 1996),
+                      round = TRUE,
+                      step = 1,
+                      animate = TRUE
+                      ),
+          
+          selectInput("select", 
+                      label = h3("Select an Age Range"),
+                      choices = only.ages, 
+                      selected = only.ages[1])
+        ),
+        mainPanel(
+          plotlyOutput("GlobalMap")
+        )
+      )
+      ),
+    tabPanel("Internation Labor Scatterplot",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("select2", 
+                             label = h3("Select a Country"),
+                             choices = only.countries, 
+                             selected = only.countries[1])
+               ),
+               mainPanel(
+                 plotlyOutput("PieChart")
+               )
+             )
+             )
     )
-  )
 )
 )
