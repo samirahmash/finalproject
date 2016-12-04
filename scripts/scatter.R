722bb880e1f2eaa9ea6a783f03b2a3185a7b0e2a
eyeneye <natbombardier@gmail.com>
2016-12-03 16:56:30 -0800

    made changes

--- scripts/scatter.R
+++ scripts/scatter.R
@@ -4,9 +4,19 @@
 # load in packages
 library(dplyr)
 
+source('./scripts/datafunctions.R')
+
+# data to test out our function
 data <- read.csv("./data/ilodata.csv")
 data <- filter(data, !is.na(Obs_Value))
 
 # pare down columns in dataframe to just country, sex, age, urban status, year, and unemployment
 short.data <- select(data, Country_Label, Country_Code, Sex_Item_Label, 
                      Classif1_Item_Label, Classif2_Item_Label, Time, Obs_Value)
+
+# create a plotly scatterplot that plots obs value for urban vs rural
+
+
+# first things first let's join some tables!
+
+table1 <- FilterMapData("Urban", "Total", 1990, 2015, "Total")
