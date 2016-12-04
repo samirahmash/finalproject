d247073251e885f01c8e4311c6e321875a621730
Gabe Bizar <Gabe Bizar>
2016-12-03 17:35:09 -0800

    Made the scatterplot and finalized the joined data table

---
+++ scripts/scatterdata.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(plotly)
+
+UnemployedScatter <- function(df, year1, year2, gender, age) {
+  short.data <- select(df, Country_Label, Country_Code, Sex_Item_Label, 
+                       Classif1_Item_Label, Classif2_Item_Label, Time, Obs_Value)
+  short.data <- filter(short.data, 
+                       Time >= year1,
+                       Time >= year2,
+                       Sex_Item_Label == gender, 
+                       age == Classif1_Item_Label) %>%
+    group_by(Country_Label)
+  urban.data <- short.data %>%
+    filter(Classif2_Item_Label == "Urban") %>%
+           summarize(urban.mean = mean(Obs_Value))
+  rural.data <- short.data %>%
+    filter(Classif2_Item_Label == "Rural") %>%
+    summarize(rural.mean = mean(Obs_Value))
+  joined.data <- left_join(urban.data, rural.data, by = "Country_Label")
+  joined.data <- joined.data %>% filter(!is.na(urban.mean)) %>%
+    filter(!is.na(rural.mean))
+  
+  return(joined.data)
+}
---
+++ scripts/scatterunemployent.R
@@ -0,0 +1,31 @@
+library(plotly)
+library(dplyr)
+
+
+UrbanRuralScatter <- function(df) {
+  df$hover = with(df, paste("Country:", Country_Label, '<br>', 
+                            "Percentage of Urban Unemployment:", urban.mean, '<br>',
+                            "Percentage of Rural Unemployment:", rural.mean, '<br>',
+                            "Combined Unemployment:", urban.mean + rural.mean)
+  )
+  return(
+    plot_ly(df, 
+      x = ~urban.mean,
+      y = ~rural.mean,
+      text = ~hover,
+      type = "scatter",
+      mode = "markers",
+      marker = list(size = 10,
+                    color = 'Gold',
+                    line = list(width = 2,
+                                color = 'Purple'))) %>% 
+      layout(title = 'Comparing Urban and Rural Unemployment',
+             yaxis = list(title = "Rural Unemployment",
+                          zeroline = FALSE),
+             xaxis = list(title = "Urban Unemployement",
+                          zeroline = FALSE))
+    )
+  
+}
+
+
