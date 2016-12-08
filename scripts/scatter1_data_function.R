9d0a1e7f55824713100ec38b581646848e3b6b5d
samirahmash <smashm12@uw.edu>
2016-12-07 21:58:29 -0800

    Finished commenting function code.

--- scripts/scatter1_data_function.R
+++ scripts/scatter1_data_function.R
@@ -6,15 +6,21 @@ library(plotly)
 UnemployedScatter <- function(df, year1, year2, gender, age) {
   short.data <- select(df, Country_Label, Country_Code, Sex_Item_Label, 
                        Classif1_Item_Label, Classif2_Item_Label, Time, Obs_Value)
+  
+  # Gives values for parameters to grab urban and rural data for scatter plot.
   short.data <- filter(short.data, 
                        Time >= year1,
                        Time >= year2,
                        Sex_Item_Label == gender, 
                        age == Classif1_Item_Label) %>%
     group_by(Country_Label)
+  
+  # Gives mean unemployment percentage for each country in urban areas.
   urban.data <- short.data %>%
     filter(Classif2_Item_Label == "Urban") %>%
            summarize(urban.mean = mean(Obs_Value))
+  
+  # Gives mean unemployment percentage for each country in rural areas.
   rural.data <- short.data %>%
     filter(Classif2_Item_Label == "Rural") %>%
     summarize(rural.mean = mean(Obs_Value))
@@ -24,5 +30,6 @@ UnemployedScatter <- function(df, year1, year2, gender, age) {
   joined.data <- joined.data %>% filter(!is.na(urban.mean)) %>%
     filter(!is.na(rural.mean))
   
+  # Returns joined pared datasets to use for interactive scatterplot.
   return(joined.data)
 }
